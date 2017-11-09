bf7006a5c05643841b25290b0bea8ce27839dc59
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-11-09 07:51:22 -0800

    created the needed service as structured

---
+++ src/app/shared/activities.ts
@@ -0,0 +1,13 @@
+import { IActivity } from './activity.model';
+
+
+export const SAVED_ACTIVITIES: IActivity[] = [
+	{
+		"id": 1,
+		"name" : "Main Bike Trails",
+		"date" : new Date('06/01/2017'),
+		"comments": "Nice day, cool temps",
+		"distance": 6.2,
+		"gpxData": "../../assets/gpx/1.gpx"
+	}
+]
