41e11ca986f86775662a058225b8aae65c74c3e4
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-11-09 09:15:03 -0800

    imported google fonts, styled the appplication, STOPPED AT 38:00

--- src/app/shared/activities.ts
+++ src/app/shared/activities.ts
@@ -5,9 +5,25 @@ export const SAVED_ACTIVITIES: IActivity[] = [
 	{
 		"id": 1,
 		"name" : "Main Bike Trails",
-		"date" : new Date('06/01/2017'),
+		"date" : new Date('06/05/2017'),
 		"comments": "Nice day, cool temps",
 		"distance": 16.2,
+		"gpxData": "../../assets/gpx/1.gpx"
+	},
+	{
+		"id": 2,
+		"name" : "Main Bike Trails",
+		"date" : new Date('07/02/2017'),
+		"comments": "Nice Weather",
+		"distance": 3.2,
+		"gpxData": "../../assets/gpx/1.gpx"
+	},
+	{
+		"id": 3,
+		"name" : "Main Bike Trails",
+		"date" : new Date('9/12/2017'),
+		"comments": "Rain Eveywhere",
+		"distance": 5.2,
 		"gpxData": "../../assets/gpx/1.gpx"
 	}
 ]
