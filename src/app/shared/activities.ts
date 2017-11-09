4946e551b4d001f5f675fcbedde25a00e39208ad
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-11-09 08:24:50 -0800

    retrived the data in a table and tested the results and the methods

--- src/app/shared/activities.ts
+++ src/app/shared/activities.ts
@@ -7,7 +7,7 @@ export const SAVED_ACTIVITIES: IActivity[] = [
 		"name" : "Main Bike Trails",
 		"date" : new Date('06/01/2017'),
 		"comments": "Nice day, cool temps",
-		"distance": 6.2,
+		"distance": 16.2,
 		"gpxData": "../../assets/gpx/1.gpx"
 	}
 ]
