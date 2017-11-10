46bea608244ed023b83aecf4e277754341ca57bd
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-11-10 08:33:20 -0800

    fixed the typo in map.component.ts

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -22,8 +22,17 @@ export class MapComponent implements OnInit {
 
 
 	ngOnInit() {
-		this.activity = this._mapService,getActivity(
+		this.activity = this._mapService.getActivity(
 			+this._route.snapshot.params['id'])
+	}
+
+	ngAfterViewInit() {
+		this._mapService.plotActivity(+this._route.snapshot.params['id']);
+		this.activityName = this.activity.name;
+		this.activityComments = this.activity.comments;
+		this.activityDistance = this.activity.distance;
+		this.ActivityDate = this.activity.Date;
+		this.gpx = this.activity.gpxData;
 	}
 
 }
